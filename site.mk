1a5fa566aaba044159629ea1df3b8da74bf4a1a6
Tom <tsiener@gmx.de>
2021-11-19 12:59:06 +0100

    Revert "v046 mit gluon2021.1.1"
    This reverts commit c350a55a48e78e32181977d06bb4c25868043b32.

--- site.mk
+++ site.mk
@@ -193,7 +193,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffaw-v046
+DEFAULT_GLUON_RELEASE := ffaw-v045
 
 
 #       GLUON_RELEASE
